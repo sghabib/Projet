3756040aae0fd58f5cf6feef435f44ce0202c7ac
Émile <mile@192.168.138.115>
2018-02-01 14:33:57 -0500

    Ajout de la classe CV

---
+++ cv.java
@@ -0,0 +1,13 @@
+
+public class cv {
+
+	public cv() {
+		
+	}
+	
+	private static void afficher() {
+		System.out.println("Voici mon CV :\n");
+		System.out.println("Nom :\t\tSalutation");
+		System.out.println("Prénom :");
+	}
+}
