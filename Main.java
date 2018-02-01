29b3328a09f338e0a90b11cb625fb32d1b25f113
Jane-Mary Menassa <jane-mary_love@hotmail.com>
2018-02-01 15:15:56 -0500

    Creation de la methode CreateCV avec l'object cvJane

--- Main.java
+++ Main.java
@@ -4,8 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Bienvenue chez barette!");
+		
+		createCV();
+		
 
-
+	}
+	
+	private static void createCV(){
+		
+		Cv cvJane = new Cv("Menassa", "Jane-Mary", "Diplôme d'étude secondaire", "Pas d'experience de travail", "Bonne élève, ponctuel, travaillante, professionel, sociable, dévouée", "Travailler dans un environement sain avec des collegues professionel.");
+		
 	}
 
 
