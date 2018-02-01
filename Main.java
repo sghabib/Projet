a529d0bb310c910c6b318e2f2a13eb921f805e7e
Jane-Mary Menassa <jane-mary_love@hotmail.com>
2018-02-01 15:18:37 -0500

    Creation  des objets cvEmile et cvPhilippe

--- Main.java
+++ Main.java
@@ -13,7 +13,8 @@ public class Main {
 	private static void createCV(){
 		
 		Cv cvJane = new Cv("Menassa", "Jane-Mary", "Diplôme d'étude secondaire", "Pas d'experience de travail", "Bonne élève, ponctuel, travaillante, professionel, sociable, dévouée", "Travailler dans un environement sain avec des collegues professionel.");
-		
+		Cv cvEmile = new Cv("","","","","","");
+		Cv cvPhilippe = new Cv("","","","","","");
 	}
 
 
