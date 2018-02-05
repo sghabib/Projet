8ab060793b097e50e06149499a33460ee03cd987
Émile <mile@192.168.138.115>
2018-02-05 17:17:55 -0500

    Meilleure structure du code

--- Main.java
+++ Main.java
@@ -3,20 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bienvenue chez barette!");
+		System.out.println("Bienvenue chez Barette!");
+		System.out.println();
+		System.out.println();
 		
-		createCV();
-		
-
-	}
-	
-	private static void createCV(){
-		
-		CV cvJane = new CV("Menassa", "Jane-Mary", "Dipl�me d'\u00e9tude secondaire", "Pas d'experience de travail", "Bonne \u00e9l�ve, ponctuel, travaillante, professionel, sociable, d\u00e9vou\u00e9e", "Travailler dans un environement sain avec des collegues professionel.");
+		CV cvJane = new CV("Menassa", "Jane-Mary", "Dipl\u00f4me d'\u00e9tude secondaire", "Pas d'exp\u00e9rience de travail", "Bonne \u00e9l\u00e8ve, ponctuel, travaillante, professionelle, sociable, d\u00e9vou\u00e9e", "Travailler dans un environement sain avec des coll\u00e8gues professionel.");
 		CV cvPhilippe = new CV("","","","","","");
-		CV cvEmile = new CV("Brunelle", "\u00c9mile", "Soutien informatique", "4 ans au soutien technique", "Windows Server, D\u00e9pannage de PC", "Bonnes conditions");
+		CV cvEmile = new CV("Brunelle", "\u00c9mile", "Soutien informatique", "4 ans au soutien technique", "Windows Server, D\u00e9pannage de PC", "Bonnes conditions de travail.");
 		
+		cvJane.afficher();
+		cvPhilippe.afficher();
+		cvEmile.afficher();
 	}
-
 
 }
